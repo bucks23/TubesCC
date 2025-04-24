aa995f5bbb39828baa718ead0d4b9d0cb2984ebd
rifkinasss <anashirulrifki@gmail.com>
2025-04-25 06:14:12 +0800

    Update FrontEnd

---
+++ frontend/main/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./layouts/Navbar";
+import Beranda from "./Beranda";
+import Footer from "./layouts/Footer";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Beranda />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
