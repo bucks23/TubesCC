7128234853f8822c7c1d26856a52186e0e44407b
rifkinasss <anashirulrifki@gmail.com>
2025-06-11 13:01:30 +0800

    tambah url

--- frontend/main/src/components/Room.jsx
+++ frontend/main/src/components/Room.jsx
@@ -120,7 +120,7 @@ function RoomItem({ room }) {
   const handleBookingClick = () => {
     if (room.status !== "Available") return;
 
-    navigate("/booking");
+    navigate("/booking/" + room.id);
   };
 
   return (
