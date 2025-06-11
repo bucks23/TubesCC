7bedc674075472b9111dc5fcc75865b86e920c92
galurpermana <galurpermana12@gmail.com>
2025-06-11 10:42:00 +0800

    typo fix

--- frontend/main/src/components/Admin/Home.jsx
+++ frontend/main/src/components/Admin/Home.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import Sidebar from "./layouts/sidebar";
+import Sidebar from "./layouts/Sidebar";
 import Dashboard from "./Dashboard";
 
 const Home = () => {
